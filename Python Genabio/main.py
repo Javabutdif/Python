62978914bcb3a77f8ae3d185ea31b15f6f53ff59
Jems <116171287+Javabutdif@users.noreply.github.com>
2023-10-09 07:36:31 +0800

    Detect whether file exist or not

--- Python Genabio/main.py
+++ Python Genabio/main.py
@@ -5,6 +5,7 @@
 """
 
 from api import *
+from pathlib import Path
 
 def displaymenu()->None:
 	header("     Main Menu     ")
@@ -18,25 +19,29 @@ def displaymenu()->None:
 	)
 	[print(menu) for menu in menuitems]
 
+
 def main()->None:
-	menuoptions:dict = {
+    path:str = Path('student.csv')
+    if path.is_file() == False:
+        f = open("student.csv", "x")
+    menuoptions:dict = {
 		1:addStudent,
 		2:findStudent,
 		3:deleteStudent,
 		4:updateStudent,
 		5:displayAllStudent,
 		0:quit,
-	}
-	option:int = -1
-	while option != 0:
-		displaymenu()
-		option = int(input("Enter Option (0...5):"))
-		selected_option = menuoptions.get(option,"invalid")
-		if selected_option!= "invalid":
-			selected_option()
-		else:
-			print("Invalid Option")
-		input("Press any key to continue...")
+    }
+    option:int = -1
+    while option != 0:
+        displaymenu()
+        option = int(input("Enter Option (0...5):"))
+        selected_option = menuoptions.get(option,"invalid")
+        if selected_option!= "invalid":
+            selected_option()
+        else:
+            print("Invalid Option")
+        input("Press any key to continue...")
 		
 if __name__=="__main__":
 	main()
